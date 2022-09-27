446855485d7a26c6a693233ea0bbe9c0aaa7ba5d
Harm Putman <harm@high-five.dev>
2022-09-27 23:27:23 +0200

    Cleanup salts script

--- scripts/salts.sh
+++ scripts/salts.sh
@@ -5,11 +5,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while read -r LINE
 do
     KEY=$(TMP=${LINE:8:19}; echo ${TMP%\'*})
-    VALUE=$(TMP=${LINE:28:-3}; echo $TMP|sed -e 's/\\/\\\\/g')
-    # echo $KEY;
-    # echo $VALUE;
-    # php -R 'echo preg_replace("");' $SCRIPT_DIR/../.env
-    # echo "s|$KEY=.*|$KEY='${VALUE}'|" $SCRIPT_DIR/../.env
-    # sed -i -r "s|${KEY}=.*|${KEY}='${VALUE}'|" $SCRIPT_DIR/../.env
+    VALUE=${LINE:28:-3}
     echo "${KEY}='${VALUE}'" >> $SCRIPT_DIR/../.env
 done <<<"$(wget https://api.wordpress.org/secret-key/1.1/salt/ -q -O -)"
